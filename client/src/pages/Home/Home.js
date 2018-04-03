cf60c99c6e328c1e160ebd4b6a86015e9120a936
johnmolin91 <john.molin91@gmail.com>
2018-04-03 16:41:53 -0400

    turn in

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Jumbotron from "../../components/Jumbotron";
-import API from "../../utils/api";
+import API from "../../utils/API";
 import { Col, Row, Container } from "../../components/Grid";
 import { List, ListItem } from "../../components/List";
 import { Input, TextArea, FormBtn } from "../../components/Form";
